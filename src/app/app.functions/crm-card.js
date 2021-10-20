d4c3be2bfdff81c1358037ed51780cb3ecbd081f
Ben Anderson <banderson@hubspot.com>
2021-10-20 14:59:46 -0400

    Clean up main fetch logic

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 
-async function getMembersOfList(listId, token) {
+/**
+ * Hardcoded IDs and other values that should change per portal
+ */
+const config = {
+  portalId: 99553819,
+  membershipListId: 34,
+};
+
+async function isMemberOfList(vid, listId, token) {
   return axios
     .get(`https://api.hubspotqa.com/contacts/v1/lists/${listId}/contacts/all`, {
       headers: {
@@ -8,16 +16,19 @@ async function getMembersOfList(listId, token) {
       },
     })
     .then(result => {
-      return result.data.contacts;
+      const contacts = result.data.contacts;
+      return !!contacts.filter(contact => `${contact.vid}` === `${vid}`).length;
     });
 }
 
-async function isMemberOfList(vid, listId, token) {
-  return getMembersOfList(listId, token).then(contacts => {
-    return !!contacts.filter(contact => `${contact.vid}` === `${vid}`).length;
-  });
-}
-
+/**
+ * Main "fetch function": see "webhooks" tab in the official docs:
+ * https://developers.hubspot.com/docs/api/crm/extensions/custom-cards
+ *
+ * Note: for the "sections" config that renders custom components, there's early
+ * documenation being gathered in the project repo for now:
+ * https://git.hubteam.com/HubSpot/ui-extensibility#shape-of-the-json-payload
+ */
 exports.main = async (context = {}, sendResponse) => {
   const {
     associatedObjectId,
@@ -25,11 +36,9 @@ exports.main = async (context = {}, sendResponse) => {
     hs_content_membership_registered_at,
   } = context;
 
-  const listId = 34;
-
   const isMember = await isMemberOfList(
     associatedObjectId,
-    listId,
+    config.membershipListId,
     context.secrets.PRIVATE_APP_ACCESS_TOKEN
   );
 
@@ -38,7 +47,15 @@ exports.main = async (context = {}, sendResponse) => {
   let settingsAction;
   let primaryAction;
 
-  if (!hs_emailconfirmationstatus || hs_emailconfirmationstatus === 'Confirmation Pending') {
+  /**
+   * Use Case: this is the rare exception where the user has not opted in to
+   * receive emails from HubSpot. This part is pretty contrived, but it lets us
+   * highlight the ability to trigger a workflow to execute any custom logic
+   */
+  if (
+    !hs_emailconfirmationstatus ||
+    hs_emailconfirmationstatus === 'Confirmation Pending'
+  ) {
     sections = [
       {
         type: 'alert',
@@ -67,106 +84,56 @@ exports.main = async (context = {}, sendResponse) => {
         text: `Notify Contact`,
       },
     ];
-  } else if (isMember && hs_content_membership_registered_at) {
-    sections = [
-      {
-        type: 'alert',
-        titleText: 'Fully Registered',
-        bodyText: 'This contact has full access to private content.',
-        variant: 'success',
-      },
-    ];
-    actions = [
-      {
-        type: 'CONFIRMATION_SERVERLESS_ACTION_HOOK',
-        serverlessFunction: 'revoke',
-        confirmationMessage:
-          "Are you sure you want to revoke this user's site access?",
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-        associatedObjectProperties: ['protected_account'],
-        label: 'Revoke access',
-      },
-      {
-        type: 'IFRAME',
-        width: 1200,
-        height: 750,
-        uri: 'https://app.hubspotqa.com/contacts/99553819/objects/0-1/views/1003073/list',
-        label: 'View Access List',
-      },
-      {
-        type: 'IFRAME',
-        width: 1200,
-        height: 750,
-        uri: 'https://knowledge.hubspot.com/website-pages/require-member-registration-to-access-private-content',
-        label: 'Knowledge Base',
-      },
-    ];
-    settingsAction = {
-      type: 'IFRAME',
-      width: 1200,
-      height: 750,
-      uri: 'https://app.hubspotqa.com/content/99553819/edit/43256011354/settings',
-      label: 'Settings',
-    };
 
+    /**
+     * Use Case: contact is a member of the access list, they can
+     * access the members-only reporting section of the site
+     */
   } else if (isMember) {
-    sections = [
-      {
-        type: 'alert',
-        titleText: 'Awaiting Registration',
-        bodyText: 'Contact was granted access, but has not yet registered.',
-        variant: 'warning',
-      },
-      {
-        type: 'button',
-        onClick: {
-          type: 'SERVERLESS_ACTION_HOOK',
-          serverlessFunction: 'workflow',
-          label: 'Send Reminder',
+    if (hs_content_membership_registered_at) {
+      // They've actually registered for the site
+      sections = [
+        {
+          type: 'alert',
+          titleText: 'Fully Registered',
+          bodyText: 'This contact has full access to private content.',
+          variant: 'success',
         },
-        text: `Send Reminder`,
-      },
-    ];
-    actions = [
-      {
-        type: 'CONFIRMATION_SERVERLESS_ACTION_HOOK',
-        serverlessFunction: 'revoke',
-        confirmationMessage:
-          "Are you sure you want to revoke this user's site access?",
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-        associatedObjectProperties: ['protected_account'],
-        label: 'Revoke access',
-      },
-      {
-        type: 'IFRAME',
-        width: 1200,
-        height: 750,
-        uri: 'https://app.hubspotqa.com/contacts/99553819/objects/0-1/views/1003073/list',
-        label: 'View Access List',
-      },
-      {
-        type: 'IFRAME',
-        width: 1200,
-        height: 750,
-        uri: 'https://knowledge.hubspot.com/website-pages/require-member-registration-to-access-private-content',
-        label: 'Knowledge Base',
-      },
-    ];
-    settingsAction = {
-      type: 'IFRAME',
-      width: 1200,
-      height: 750,
-      uri: 'https://app.hubspotqa.com/content/99553819/edit/43256011354/settings',
-      label: 'Settings',
-    };
+      ];
+    } else {
+      // They've received the registration email, but haven't registered yet
+      sections = [
+        {
+          type: 'alert',
+          titleText: 'Awaiting Registration',
+          bodyText: 'Contact was granted access, but has not yet registered.',
+          variant: 'warning',
+        },
+        {
+          type: 'button',
+          onClick: {
+            type: 'SERVERLESS_ACTION_HOOK',
+            serverlessFunction: 'workflow',
+            label: 'Send Reminder',
+          },
+          text: `Send Reminder`,
+        },
+      ];
+    }
+
+    actions = getDropdownActionList();
+    settingsAction = getContentSettingsIFrame();
   } else {
+    /**
+     * Use Case: contact is NOT a member of the access list, so no
+     * access to the members-only reporting section at all
+     */
     sections = [
       {
         type: 'alert',
         titleText: 'Public content only',
-        bodyText: 'This contact has not registered, so can only access public pages.',
+        bodyText:
+          'This contact has not registered, so can only access public pages.',
         variant: 'info',
       },
       {
@@ -201,3 +168,42 @@ exports.main = async (context = {}, sendResponse) => {
     primaryAction,
   });
 };
+
+function getContentSettingsIFrame() {
+  return {
+    type: 'IFRAME',
+    width: 1200,
+    height: 750,
+    uri: `https://app.hubspotqa.com/content/${config.portalId}/edit/43256011354/settings`,
+    label: 'Settings',
+  };
+}
+
+function getDropdownActionList() {
+  return [
+    {
+      type: 'CONFIRMATION_SERVERLESS_ACTION_HOOK',
+      serverlessFunction: 'revoke',
+      confirmationMessage:
+        "Are you sure you want to revoke this user's site access?",
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+      associatedObjectProperties: ['protected_account'],
+      label: 'Revoke access',
+    },
+    {
+      type: 'IFRAME',
+      width: 1200,
+      height: 750,
+      uri: `https://app.hubspotqa.com/contacts/${config.portalId}/objects/0-1/views/1003073/list`,
+      label: 'View Access List',
+    },
+    {
+      type: 'IFRAME',
+      width: 1200,
+      height: 750,
+      uri: 'https://knowledge.hubspot.com/website-pages/require-member-registration-to-access-private-content',
+      label: 'Knowledge Base',
+    },
+  ];
+}
